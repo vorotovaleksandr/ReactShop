833b7ab2caaa190ebd87db573a81cb18b280a9c8
mgujuman <mgujuman.exceedteam@gmail.com>
2019-10-24 11:32:40 +0300

    add main page

---
+++ src/components/main/main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './main.css';
+
+const Main = () => {
+  return (
+    <div className="grid-container">
+      <div className="header"> Магазин мелочей</div>
+      <div className="login">login</div>
+      <div className="content">
+        
+      </div>
+      <div className="nav">
+        <div>1 ссылка</div>
+        <div>2 ссылка</div>
+      </div>
+      <div className="footer"> exceed-team 2019 </div>
+    </div>
+  );
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Main from './components/main/main';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
 
 serviceWorker.unregister();
